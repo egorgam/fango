0a8d5543b7e444a463c6b4356c4313bff2742f3c
Egor Gamazin <egorgamazin@yandex.ru>
2024-01-08 07:50:51 +0300

    edit paginator; move security;

--- fango/depends.py
+++
@@ -1,7 +0,0 @@
-from fastapi import Request
-
-from fango.paginator.cursor import CursorPagination
-
-
-def get_paginator(request: Request):
-    return CursorPagination(request)
--- fango/paginator/cursor.py
+++ fango/paginator/cursor.py
@@ -1,10 +1,15 @@
+import typing
+
+if typing.TYPE_CHECKING:
+    from django.db.models.query import ValuesQuerySet
+    from django.db.models import QuerySet
+    from fastapi import Request
+
 from base64 import b64decode, b64encode
 from collections import namedtuple
 from typing import TypeVar
 from urllib import parse
 
-from asgiref.sync import sync_to_async
-from django.db.models import QuerySet
 from django.utils.encoding import force_str
 
 from fango.paginator.schemas import Page
@@ -14,22 +19,18 @@ T = TypeVar("T")
 Cursor = namedtuple("Cursor", ["offset", "reverse", "position"])
 
 
-def replace_query_param(url, key, val):
-    (scheme, netloc, path, query, fragment) = parse.urlsplit(force_str(url))
-    query_dict = parse.parse_qs(query, keep_blank_values=True)
-    query_dict[force_str(key)] = [force_str(val)]
-    query = parse.urlencode(sorted(query_dict.items()), doseq=True)
-    return parse.urlunsplit((scheme, netloc, path, query, fragment))
-
-
 class CursorPagination:
-    page_size = 15
-    ordering = ("id",)
+    """
+    DRF like cursor pagination class with sync and async support.
 
-    def __init__(self, request) -> None:
+    """
+
+    def __init__(self, request: "Request", page_size: int, ordering: tuple) -> None:
         self.request = request
-        self.cursor = self.decode_cursor()
+        self.cursor = self._decode_cursor()
         self.has_more_data = False
+        self.page_size = page_size
+        self.ordering = ordering
 
     def _positive_int(self, integer_string: str, strict: bool = False, cutoff: int | None = None) -> int:
         ret = int(integer_string)
@@ -39,12 +40,8 @@ class CursorPagination:
             return min(ret, cutoff)
         return ret
 
-    def decode_cursor(self) -> Cursor | None:
-        offset_cutoff = 1000
-        invalid_cursor_message = "Invalid cursor"
-        cursor_query_param = "cursor"
-
-        encoded = self.request.query_params.get(cursor_query_param)
+    def _decode_cursor(self) -> Cursor | None:
+        encoded = self.request.query_params.get("cursor")
         if encoded is None:
             return None
 
@@ -53,21 +50,20 @@ class CursorPagination:
             tokens = parse.parse_qs(querystring, keep_blank_values=True)
 
             offset = tokens.get("o", ["0"])[0]
-            offset = self._positive_int(offset, cutoff=offset_cutoff)
+            offset = self._positive_int(offset, cutoff=1000)
 
             reverse = tokens.get("r", ["0"])[0]
             reverse = bool(int(reverse))
 
             position = tokens.get("p", [None])[0]
         except (TypeError, ValueError):
-            raise Exception(invalid_cursor_message)
+            raise Exception("Invalid cursor")
 
         return Cursor(offset=offset, reverse=reverse, position=position)
 
-    async def encode_cursor(self, cursor: Cursor) -> str:
-        cursor_query_param = "cursor"
-
+    def _encode_cursor(self, cursor: Cursor) -> str:
         tokens = {}
+
         if cursor.offset != 0:
             tokens["o"] = str(cursor.offset)
         if cursor.reverse:
@@ -77,49 +73,69 @@ class CursorPagination:
 
         querystring = parse.urlencode(tokens, doseq=True)
         encoded = b64encode(querystring.encode("ascii")).decode("ascii")
-        return replace_query_param(self.request.url, cursor_query_param, encoded)
 
-    async def paginate_queryset(self, queryset: QuerySet) -> Page:
+        (scheme, netloc, path, query, fragment) = parse.urlsplit(force_str(self.request.url))
+        query_dict = parse.parse_qs(query, keep_blank_values=True)
+        query_dict[force_str("cursor")] = [force_str(encoded)]
+        query = parse.urlencode(sorted(query_dict.items()), doseq=True)
+        return parse.urlunsplit((scheme, netloc, path, query, fragment))
+
+    def paginate_sync(self, queryset: "QuerySet | ValuesQuerySet") -> "Page":
         if self.cursor:
             offset = self.cursor.offset
-            limit = offset + self.page_size + 1
         else:
             offset = 0
-            limit = self.page_size + 1
 
-        page_queryset = queryset[offset:limit]
-        page_data = await sync_to_async(list)(page_queryset)
+        if self.cursor and self.cursor.position is not None:
+            queryset = queryset.filter(id__gt=int(self.cursor.position))
 
-        if len(page_data) > self.page_size:
+        queryset.query.set_limits(high=offset + self.page_size + 1)
+
+        data = tuple(queryset)
+        return self.get_page(data)
+
+    async def paginate_async(self, queryset: "QuerySet | ValuesQuerySet") -> "Page":
+        if self.cursor:
+            offset = self.cursor.offset
+        else:
+            offset = 0
+
+        if self.cursor and self.cursor.position is not None:
+            queryset = queryset.filter(id__gt=int(self.cursor.position))
+
+        queryset.query.set_limits(high=offset + self.page_size + 1)
+
+        data = tuple([x async for x in queryset])
+        return self.get_page(data)
+
+    def get_page(self, data: tuple) -> Page:
+        if len(data) > self.page_size:
             self.has_more_data = True
-            page_data = page_data[:-1]
+            data = data[:-1]
         else:
             self.has_more_data = False
 
-        return await self.get_paginated_response(page_data)
-
-    async def get_paginated_response(self, data: list[QuerySet]) -> Page:
         return Page(
-            previous=await self.get_previous_link(),
-            next=await self.get_next_link(),
+            next=self.get_next_link(),
+            previous=self.get_previous_link(),
             results=data,
         )
 
-    async def get_previous_link(self) -> str | None:
+    def get_previous_link(self) -> str | None:
         if self.cursor:
             if self.cursor.offset < 2 * self.page_size:
-                return await self.encode_cursor(Cursor(offset=0, reverse=False, position=self.page_size))
+                return self._encode_cursor(Cursor(offset=0, reverse=False, position=self.page_size))
 
             position = self.cursor.offset - self.page_size
-            return await self.encode_cursor(Cursor(offset=position, reverse=False, position=self.page_size))
+            return self._encode_cursor(Cursor(offset=position, reverse=False, position=self.page_size))
 
-    async def get_next_link(self) -> str | None:
+    def get_next_link(self) -> str | None:
         if self.cursor:
             new_offset = self.cursor.offset
         else:
             new_offset = 0
 
         if self.has_more_data:
-            return await self.encode_cursor(
+            return self._encode_cursor(
                 Cursor(offset=new_offset + self.page_size, reverse=False, position=new_offset + self.page_size)
             )
