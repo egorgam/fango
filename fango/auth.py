89b06432762a6af605e766227f9efa53a900be8a
Egor Gamazin <egorgamazin@yandex.ru>
2024-01-15 21:04:24 +0300

    fix type annotations;

--- fango/auth.py
+++ fango/auth.py
@@ -23,7 +23,7 @@ def decode_token(auth: str) -> dict:
         raise HTTPException(status_code=403, detail=str(e))
 
 
-def get_user(request: "Request") -> "User | None":
+def get_user(request: "Request") -> "User":
     """
     Return User instance by token
 
@@ -32,7 +32,7 @@ def get_user(request: "Request") -> "User | None":
     return User.objects.get(id=payload["user_id"])
 
 
-async def get_user_async(request: "Request") -> "User | None":
+async def get_user_async(request: "Request") -> "User":
     """
     Return User instance by token async
 
